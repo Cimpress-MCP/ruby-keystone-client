8adec945485997d8101613742563e8315e692124
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:14:22 +0000

    Remove Duplicate Client Specs
    Remove specs for the client as they will be superceded by the upcoming
    Manager specs.

--- spec/features/client_spec.rb
+++ spec/features/client_spec.rb
@@ -19,14 +19,6 @@ describe "Keystone V2.0 client" do
       expect(client).to be_instance_of(Keystone::V2_0::Client)
     end
 
-    it "contains managers for each query type" do
-      expect(client.users).to     be_instance_of(Keystone::V2_0::Manager::User)
-      expect(client.roles).to     be_instance_of(Keystone::V2_0::Manager::Role)
-      expect(client.tenants).to   be_instance_of(Keystone::V2_0::Manager::Tenant)
-      expect(client.services).to  be_instance_of(Keystone::V2_0::Manager::Service)
-      expect(client.endpoints).to be_instance_of(Keystone::V2_0::Manager::Endpoint)
-    end
-
     it "dynamically creates manager methods for each query type" do
       expect(client).to respond_to(:users)
       expect(client).to respond_to(:roles)
