370e2cec95dc1d1012b93294df2037a11269e5ef
Justin Karimi <jekhokie@gmail.com>
2015-01-22 16:07:46 +0000

    Attach Managers to Client
    Add specs and attach the query managers to the Keystone client
    instance.

--- lib/keystone/v2_0/client.rb
+++ lib/keystone/v2_0/client.rb
@@ -1,4 +1,9 @@
 require "keystone/v2_0/version"
+require "keystone/v2_0/manager/user"
+require "keystone/v2_0/manager/role"
+require "keystone/v2_0/manager/tenant"
+require "keystone/v2_0/manager/service"
+require "keystone/v2_0/manager/endpoint"
 
 module Keystone
   module V2_0
@@ -9,12 +14,27 @@ module Keystone
       attr_accessor :tenant_name
       attr_accessor :auth_url
 
+      # query providers
+      attr_accessor :users
+      attr_accessor :roles
+      attr_accessor :tenants
+      attr_accessor :services
+      attr_accessor :endpoints
+
       # create a new Keystone client instance with the given credentials
-      def initialize(username: '', password: '', tenant_name: '', auth_url: '')
-        username.empty?    ? raise("Must provide a username")    : self.username    = username
-        password.empty?    ? raise("Must provide a password")    : self.password    = password
-        tenant_name.empty? ? raise("Must provide a tenant_name") : self.tenant_name = tenant_name
-        auth_url.empty?    ? raise("Must provide an auth_url")   : self.auth_url    = auth_url
+      def initialize(username, password, tenant_name, auth_url)
+        # initialize the object
+        self.username    = username
+        self.password    = password
+        self.tenant_name = tenant_name
+        self.auth_url    = auth_url
+
+        # create the initial query managers
+        self.users     = Keystone::V2_0::Manager::User.new
+        self.roles     = Keystone::V2_0::Manager::Role.new
+        self.tenants   = Keystone::V2_0::Manager::Tenant.new
+        self.services  = Keystone::V2_0::Manager::Service.new
+        self.endpoints = Keystone::V2_0::Manager::Endpoint.new
       end
     end
   end
--- spec/features/client_spec.rb
+++ spec/features/client_spec.rb
@@ -6,8 +6,17 @@ describe "Keystone V2.0 client" do
   let(:password)    { "admin" }
   let(:tenant_name) { "admin" }
   let(:auth_url)    { "http://some.host:35357/v2.0/" }
+  let(:client)      { Keystone::V2_0::Client.new(username, password, tenant_name, auth_url) }
 
   it "returns an instance when created with parameters" do
-    expect(Keystone::V2_0::Client.new(username, password, tenant_name, auth_url)).to be_instance_of(Keystone::V2_0::Client)
+    expect(client).to be_instance_of(Keystone::V2_0::Client)
+  end
+
+  it "contains managers for each query type" do
+    expect(client.users).to     be_instance_of(Keystone::V2_0::Manager::User)
+    expect(client.roles).to     be_instance_of(Keystone::V2_0::Manager::Role)
+    expect(client.tenants).to   be_instance_of(Keystone::V2_0::Manager::Tenant)
+    expect(client.services).to  be_instance_of(Keystone::V2_0::Manager::Service)
+    expect(client.endpoints).to be_instance_of(Keystone::V2_0::Manager::Endpoint)
   end
 end
