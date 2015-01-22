64c138ec6907834b4cc057b4c59dcce8d2ca0749
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:01:10 +0000

    Spec and Add BaseManager List Capability
    Spec and add the ability for the BaseManager to perform a list query
    based on the url_endpoint of the subclass that is invoking it. This
    ensures that the query is kept DRY.

--- spec/features/manager/base_spec.rb
+++ spec/features/manager/base_spec.rb
@@ -1,16 +1,46 @@
 require "spec_helper"
 require "keystone/v2_0/manager/base"
+require "rest-client"
 
 describe "Keystone V2.0 base manager" do
-  let(:url_endpoint) { "/base/endpoint" }
-  let(:auth_url)     { "http://some.host:35357/v2.0/" }
-  let(:base_client)  { Keystone::V2_0::Manager::Base.new(auth_url, url_endpoint) }
+  let(:host)          { "some.host" }
+  let(:url_endpoint)  { "/base/endpoint" }
+  let(:priv_port)     { "35357" }
+  let(:auth_url)      { "http://#{host}:#{priv_port}/v2.0/" }
+  let(:base_client)   { Keystone::V2_0::Manager::Base.new(auth_url, url_endpoint) }
+  let(:endpoint_data) { "{ \"endpoints\": [
+                              { \"adminurl\":    \"http://#{host}:#{priv_port}/v2.0\",
+                                \"service_id\":  \"876fce0975f841fdbebd8352acda75f4\",
+                                \"region\":      \"regionOne\",
+                                \"publicurl\":   \"http://#{host}:5000/v2.0\",
+                                \"enabled\":     true,
+                                \"id\":          \"a584ab022f0348ab9335fa2468960578\",
+                                \"internalurl\": \"http://#{host}:5000/v2.0\" }
+                           ]
+                         }"
+                      }
 
-  it "returns an instance when initialized with a URL" do
-    expect(base_client).to be_instance_of(Keystone::V2_0::Manager::Base)
+  describe "initialize" do
+    it "returns an instance when initialized with a URL" do
+      expect(base_client).to be_instance_of(Keystone::V2_0::Manager::Base)
+    end
+
+    it "sets the url_endpoint on create" do
+      expect(base_client.url_endpoint).to eq(url_endpoint)
+    end
   end
 
-  it "sets the url_endpoint on create" do
-    expect(base_client.url_endpoint).to eq(url_endpoint)
+  describe "list" do
+    it "returns JSON content when query is successful" do
+      FakeWeb.clean_registry
+      FakeWeb.register_uri(:get, "#{auth_url}#{url_endpoint}", :status => [ 200 ], :body => endpoint_data)
+      expect(base_client.list).to eq(JSON.parse(endpoint_data))
+    end
+
+    it "returns nil when the query is unsuccessful" do
+      FakeWeb.clean_registry
+      FakeWeb.register_uri(:get, "#{auth_url}#{url_endpoint}", :status => [ 404 ], :body => "")
+      expect(base_client.list).to eq(nil)
+    end
   end
 end
