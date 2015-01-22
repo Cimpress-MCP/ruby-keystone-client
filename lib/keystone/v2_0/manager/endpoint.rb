838a342cb4e1b5352de51723a4383b2b8cbc3832
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:56:31 +0000

    DRY Up the Managers for Common Code
    Move the parsing of the JSON data up into the base Manager to conform to
    DRY principles since the code is essentially the same minus the key used
    to identify the JSON data.

--- lib/keystone/v2_0/manager/endpoint.rb
+++ lib/keystone/v2_0/manager/endpoint.rb
@@ -4,15 +4,15 @@ module Keystone
   module V2_0
     module Manager
       class Endpoint < Keystone::V2_0::Manager::Base
-        @@url_endpoint = "/endpoints"
+        @@url_endpoint = "endpoints"
+        @@json_key     = "endpoints"
 
         def initialize(auth_url)
-          super auth_url, @@url_endpoint
+          super auth_url, @@url_endpoint, @@json_key
         end
 
         def endpoints
-          endpoints = self.class.superclass.instance_method(:list).bind(self).call
-          endpoints ? endpoints["endpoints"] : nil
+          return self.class.superclass.instance_method(:list).bind(self).call
         end
       end
     end
--- lib/keystone/v2_0/manager/tenant.rb
+++ lib/keystone/v2_0/manager/tenant.rb
@@ -4,15 +4,15 @@ module Keystone
   module V2_0
     module Manager
       class Tenant < Keystone::V2_0::Manager::Base
-        @@url_endpoint = "/tenants"
+        @@url_endpoint = "tenants"
+        @@json_key     = "tenants"
 
         def initialize(auth_url)
-          super auth_url, @@url_endpoint
+          super auth_url, @@url_endpoint, @@json_key
         end
 
         def tenants
-          tenants = self.class.superclass.instance_method(:list).bind(self).call
-          tenants ? tenants["tenants"] : nil
+          return self.class.superclass.instance_method(:list).bind(self).call
         end
       end
     end
