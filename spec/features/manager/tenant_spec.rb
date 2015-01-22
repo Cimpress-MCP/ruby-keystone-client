ba80ab7ee87c1b4fab7b83ef21444cad601c3829
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:32:19 +0000

    Spec and Add Tenant Query Capability
    Spec and add the ability to query for tenants.

--- spec/features/manager/tenant_spec.rb
+++ spec/features/manager/tenant_spec.rb
@@ -2,14 +2,41 @@ require "spec_helper"
 require "keystone/v2_0/manager/tenant"
 
 describe "Keystone V2.0 tenant manager" do
-  let(:auth_url)      { "http://some.host:35357/v2.0/" }
+  let(:host)          { "some.host" }
+  let(:priv_port)     { "35357" }
+  let(:auth_url)      { "http://#{host}:#{priv_port}/v2.0/" }
   let(:tenant_client) { Keystone::V2_0::Manager::Tenant.new(auth_url) }
+  let(:url_endpoint)  { tenant_client.url_endpoint }
+  let(:tenant_data)   { "{ \"tenants\": [
+                            { \"description\": \"Admin Tenant\",
+                              \"enabled\":     true,
+                              \"id\":          \"9958cfb44628476b8f16996e76703292\",
+                              \"name\":        \"admin\" }
+                           ]
+                         }"
+                       }
 
-  it "returns an instance when initialized with a URL" do
-    expect(tenant_client).to be_instance_of(Keystone::V2_0::Manager::Tenant)
+  describe "initialize" do
+    it "returns an instance when initialized with a URL" do
+      expect(tenant_client).to be_instance_of(Keystone::V2_0::Manager::Tenant)
+    end
+
+    it "sets the url_endpoint on create" do
+      expect(tenant_client.url_endpoint).to be_truthy
+    end
   end
 
-  it "sets the url_endpoint on create" do
-    expect(tenant_client.url_endpoint).to be_truthy
+  describe "tenants" do
+    it "returns tenants on successful query" do
+      FakeWeb.clean_registry
+      FakeWeb.register_uri(:get, "#{auth_url}#{url_endpoint}", :status => [ 200 ], :body => tenant_data)
+      expect(tenant_client.tenants).to eq(JSON.parse(tenant_data)["tenants"])
+    end
+
+    it "returns nil when query is unsuccessful" do
+      FakeWeb.clean_registry
+      FakeWeb.register_uri(:get, "#{auth_url}#{url_endpoint}", :status => [ 404 ], :body => "")
+      expect(tenant_client.tenants).to eq(nil)
+    end
   end
 end
