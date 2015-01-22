5f4d9aa9601ae458d6847ef68c1e138a44e801d9
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:25:15 +0000

    Spec and Add Service Query Capability
    Spec and add the ability to query for services.

--- lib/keystone/v2_0/manager/service.rb
+++ lib/keystone/v2_0/manager/service.rb
@@ -10,9 +10,9 @@ module Keystone
           super auth_url, @@url_endpoint
         end
 
-        def list
-          # TODO: Get complete list from Keystone
-          self
+        def services
+          services = self.class.superclass.instance_method(:list).bind(self).call
+          services ? services["services"] : nil
         end
       end
     end
