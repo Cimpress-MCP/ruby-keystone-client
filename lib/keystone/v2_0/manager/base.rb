45b9b5dcf6adca4a9e48353aff4e4d9c2441d25d
Justin Karimi <jekhokie@gmail.com>
2015-01-22 19:01:10 +0000

    Spec and Add BaseManager List Capability
    Spec and add the ability for the BaseManager to perform a list query
    based on the url_endpoint of the subclass that is invoking it. This
    ensures that the query is kept DRY.

--- lib/keystone/v2_0/manager/base.rb
+++ lib/keystone/v2_0/manager/base.rb
@@ -1,3 +1,5 @@
+require "rest-client"
+
 module Keystone
   module V2_0
     module Manager
@@ -9,6 +11,27 @@ module Keystone
         def initialize(auth_url, url_endpoint)
           self.auth_url     = auth_url
           self.url_endpoint = url_endpoint
+        end
+
+        def list
+          options                           = {}
+          options[:url]                     = self.auth_url + self.url_endpoint
+          options[:method]                  = :get
+          options[:headers]                 = {}
+          options[:headers]["X-Auth-Token"] = self.token
+          options[:headers]["User-Agent"]   = "keystone-client"
+          options[:headers]["Accept"]       = "application/json"
+          options[:headers]["Content-Type"] = "application/json"
+
+          # provide a block to ensure the response is parseable rather than
+          # having RestClient throw an exception
+          RestClient::Request.execute(options) do |response, request, result|
+            if response and response.code == 200
+              return JSON.parse(response.body)
+            else
+              return nil
+            end
+          end
         end
       end
     end
